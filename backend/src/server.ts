18e12a7c057fe5ca515201f13dc6c2ee98b48af2
Luvveer Singh Lamba <luvveerlamba@gmail.com>
2026-06-23 20:38:35 -0700

    feat(backend): backend scafoldding is completed

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+const port = process.env.PORT ?? 3000;
+
+// Call routes in teh respective modules
+
+app.listen(port, () => {
+  console.log(`API listening on http://localhost:${port}`);
+  console.log(`Server health is in http://localhost:${port}/api/health`);
+});
