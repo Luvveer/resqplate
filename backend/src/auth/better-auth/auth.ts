decbede2bbef432a3b042d5d4c2fa389cda168af
Luvveer Singh Lamba <luvveerlamba@gmail.com>
2026-06-24 03:08:44 -0700

    added better-auth schema required for it to function

---
+++ backend/src/auth/better-auth/auth.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../../db/index.js";
+import * as authSchema from "../../db/auth-schema.js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: authSchema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: process.env.BETTER_AUTH_SECRET!,
+  user: {
+    additionalFields: {},
+  },
+});
+
+export default auth;
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,14 +1,35 @@
-import { pgTable, uuid, varchar, timestamp, pgEnum } from "drizzle-orm/pg-core";
-
+import {
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+  pgEnum,
+  text,
+  jsonb,
+} from "drizzle-orm/pg-core";
+import { user } from "./auth-schema.js";
 export const userRoleEnum = pgEnum("user_role", [
-  "foodSeeker",
-  "business",
-  "admin",
+  "FOOD_SEEKER",
+  "BUSINESS",
+  "ADMIN",
 ]);
 
-export const usersTable = pgTable("users", {
+export const userStatusEnum = pgEnum("user_status", [
+  "ACTIVE",
+  "SUSPENDED",
+  "DELETED",
+]);
+
+export const ProfileTable = pgTable("profile", {
   id: uuid("id").defaultRandom().primaryKey(),
+  authId: text("authId")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
   email: varchar("email", { length: 255 }).notNull().unique(),
+  name: varchar("name", { length: 255 }).notNull(),
   role: userRoleEnum("role").notNull(),
+  status: userStatusEnum("status").default("ACTIVE").notNull(),
+  dietaryPreferences: jsonb("dietary_preferences"),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
