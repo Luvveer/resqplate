44a368b3d081bcaeeac36545c255898fbad1ffe2
Luvveer Singh Lamba <luvveerlamba@gmail.com>
2026-06-26 13:26:48 -0700

    Uppdated server.ts file to accept auth, restrants routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,27 @@
 import express from "express";
 import cors from "cors";
+import AuthRouter from "./auth/auth.routes.js";
+import RestaurantRouter from "./restaurants/restaurants.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-const port = process.env.PORT ?? 3000;
+// Call routes in the respective modules
+app.use("/api/auth", AuthRouter);
+app.use("/api/restaurants", RestaurantRouter);
 
-// Call routes in teh respective modules
+const port = process.env.PORT ?? 3000;
 
 app.listen(port, () => {
   console.log(`API listening on http://localhost:${port}`);
