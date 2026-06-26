9b2cabb2f9162da7409b3af5d17210a6ca448e11
Luvveer Singh Lamba <luvveerlamba@gmail.com>
2026-06-26 13:25:39 -0700

    Added shared types

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,62 @@
-export {};
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+  name: z.string().min(1),
+  role: z.enum(["FOOD_SEEKER", "BUSINESS", "ADMIN"]),
+});
+
+export const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(1),
+});
+
+export type SignupInput = z.infer<typeof signupSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
+
+export type UserRole = "FOOD_SEEKER" | "BUSINESS" | "ADMIN";
+export type profileStatus = "ACTIVE" | "SUSPENDED" | "DELETED";
+
+export type ProfileResponse = {
+  id: string;
+  authId: string;
+  email: string;
+  name: string;
+  role: UserRole;
+  status: profileStatus;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type RestaurantProfileResponse = {
+  id: string;
+  profileId: string;
+  businessName: string;
+  address: string;
+  city: string;
+  province: string;
+  postalCode: string;
+  phone: string | null;
+  description: string | null;
+  verificationStatus:
+    | "PENDING"
+    | "APPROVED"
+    | "REJECTED"
+    | "SUSPENDED"
+    | "INFO_REQUESTED";
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const createRestaurantSchema = z.object({
+  businessName: z.string().min(1),
+  address: z.string().min(1),
+  city: z.string().min(1),
+  province: z.string().min(1),
+  postalCode: z.string().min(1),
+  phone: z.string().optional(),
+  description: z.string().optional(),
+});
+
+export type CreateRestaurantInput = z.infer<typeof createRestaurantSchema>;
