0f3ba53f422f3c004b829772f36af369c1b20f35
DaiwikMarrott <daiwik.marrott@gmail.com>
2026-07-06 20:40:11 -0700

    feat: added the routes for the reservations for the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import AuthRouter from "./auth/auth.routes.js";
 import RestaurantRouter from "./restaurants/restaurants.routes.js";
 import AdminRouter from "./admin/admin.routes.js";
 import morgan from "morgan";
+import ReservationRouter from "./reservations/reservations.routes.js";
 
 const app = express();
 app.use(morgan("dev"));
@@ -24,7 +25,7 @@ app.get("/api/health", (req, res) => {
 app.use("/api/auth", AuthRouter);
 app.use("/api/restaurants", RestaurantRouter);
 app.use("/api/admin", AdminRouter);
-
+app.use("/api/reservations", ReservationRouter);
 const port = process.env.PORT ?? 3000;
 
 app.listen(port, () => {
