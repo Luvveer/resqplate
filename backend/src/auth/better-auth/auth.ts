e3542870300c363e643797e2367ff7571e1ca94a
Luvveer Singh Lamba <luvveerlamba@gmail.com>
2026-07-12 03:44:48 -0700

    Added trusted Origin in better auth

--- backend/src/auth/better-auth/auth.ts
+++ backend/src/auth/better-auth/auth.ts
@@ -6,6 +6,10 @@ import * as authSchema from "../../db/auth-schema.js";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [
+    "https://resqplate.pages.dev",
+    "https://resqplate-api.luvveerlamba.workers.dev",
+  ],
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: authSchema,
--- scripts/setup-vm.sh
+++ scripts/setup-vm.sh
@@ -17,4 +17,12 @@ sudo npm install -g pm2
 echo "Creating app directory..."
 mkdir -p ~/app/backend
 
+echo "Loading all secrets from secret Manager..."
+export DATABASE_URL=$(gcloud secrets versions access latest --secret=DATABASE_URL)
+export BETTER_AUTH_SECRET=$(gcloud secrets versions access latest --secret=BETTER_AUTH_SECRET)
+export BETTER_AUTH_URL=$(gcloud secrets versions access latest --secret=BETTER_AUTH_URL)
+export FRONTEND_URL=$(gcloud secrets versions access latest --secret=FRONTEND_URL)
+export DATABASE_URL=2000
+echo "Loaded everything"
+
 echo "All done"
